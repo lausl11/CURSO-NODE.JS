86277580b1431c21a9812d70c2ddc3514666ddc9
lau.sl11 <laura.laurindo@Escola.pr.gov.br>
2026-07-23 09:06:42 -0300

    aula 13.2

--- teste.sql
+++ teste.sql
@@ -17,6 +17,12 @@ CREATE TABLE avisos(
     descrisao VARCHAR(100),
     data DATE
 );
+ 
+ INSERT INTO avisos(titulo, descrisao, data)
+ VALUES
+    ("tirar lixo", "tirar lixo da cozinha,quartos e banheiros","2026-7-23"),
+    ("jojo", "jojo vai lancar na netflix","2026-9-25"),
+    ("filme de avatar", "o filme de avatar vai lancar, se quiser ver vai tenque assinar a paramont","2026-7-25");
 
 CREATE TABLE promoçoes(
     item VARCHAR(50),
