89daa847f2522c266e89e64ef74b2e599765ad00
lau.sl11 <laura.laurindo@Escola.pr.gov.br>
2026-07-23 08:47:21 -0300

    aula 13.1

--- teste.sql
+++ teste.sql
@@ -4,6 +4,14 @@ CREATE TABLE usuarios (
     idade INT
 );
 
+INSERT INTO usuarios(nome, email, idade) 
+VALUES
+    ("joao cardoso", "jocao@0email.com", 90),
+    ("gustavo", "gogo@0email.com", 63),
+    ("yamal", "yamal@0email.com", 29);
+  
+  
+
 CREATE TABLE avisos(
     titulo VARCHAR(20),
     descrisao VARCHAR(100),
