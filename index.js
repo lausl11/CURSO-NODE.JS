179166f04b29f2f7813719ee8e4fba2c15963a82
lau.sl11 <laura.laurindo@Escola.pr.gov.br>
2026-07-30 20:41:58 -0300

    aula 19

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const express = require("express");
 const app = express();
+const { engine } = require('express-handlebars');
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/html/index.html");
-});
-
-app.get("/sobre", function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html"); 
-});
-
-
-app.get("/blog", function(req, res){
-    res.send("meu blog");
-});
-
-app.get('/ola/:nome/:profissao', function(req, res){
-    res.send('Olá ' + req.params.nome + ', sua profissão é: ' + req.params.profissao)
-});
-
-app.get("/planta/:tipo/:cor", function(req, res){
-    res.send('sua planta favorita, '+ 'ela e do tipo '+req.params.tipo+', e a cor e: '+req.params.cor)
-});
+// config
+    // templade engine
+   app.engine('handlebars', engine({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars')
 
 app.listen(3000, function(){
     console.log("servidor rodando na url localhost:3000");
